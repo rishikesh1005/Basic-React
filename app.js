83262529786daa456e25f67eeb2d04cdfffd9bb2
Rishikesh <rishikeshy503@gmail.com>
2026-01-20 18:16:26 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const heading1 = React.createElement("h1",
+    {
+        id:"title1",
+    } ,
+    "Heading1"
+);
+
+const heading2 = React.createElement("h2", 
+    {
+        id:"title2",
+    }, 
+    "Heading2"
+);
+
+const container = React.createElement("div" ,
+    {
+        id:"container",
+    } ,
+    [heading1,heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
+
