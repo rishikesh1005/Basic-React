77d391d3b174dc61eeace0e4875a444aabd227c4
Rishikesh <rishikeshy503@gmail.com>
2026-01-28 23:11:05 +0530

    Get Basic of Hooks and use of useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,30 @@
 import { restaurantList } from "../constant";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
-    return(
-       <div className="restaurantList">
+    let [searchInput,setSearchInput] = useState();
 
+    return(
+      <>
+       <div>
+         <input 
+            className="search-container" 
+            placeholder="search" 
+            value={searchInput}
+            onChange={(e) => {
+                setSearchInput = e.target.value;
+            }}  
+         />
+         <button>Search</button>        
+       </div>
+       <div className="restaurantList">
         {restaurantList.map((restaurant) => {
             return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
         })}
         
        </div>
+      </>  
     )
 }
 
